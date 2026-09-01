4773ef2590f53d1356b7c949c2c5f4643ea05910
Naitik Sahu <naiitik1526@gmail.com>
2026-09-01 12:25:47 +0530

    starting

---
+++ cooked.c
@@ -0,0 +1,13 @@
+
+#include  <stdio.h> // pre processor directive
+
+int main(){
+    int a=72;
+    char star='*';
+    float pi = 3.14;  //theres  no string or boolean data type in c
+    printf("The value of a is %d\n",a);  // \n is used to print in new line
+    printf("The value of star is %c\n",star);
+    printf("The value of pi is %f\n",pi);
+    return 0 ;
+} 
+//%d is used to print integer values, %c is used to print character values, %f is used to print float values-FORMAT SPECIFIERS
