81a9d9fdd2fa4c97d7f100de7132c247af0a2c99
Naitik Sahu <naiitik1526@gmail.com>
2026-09-02 11:36:45 -0300

    studied assignment ooperators blala

--- cooked.c
+++ cooked.c
@@ -1,13 +1,51 @@
+//1st sept
+// #include  <stdio.h> // pre processor directive
 
-#include  <stdio.h> // pre processor directive
-
-int main(){
-    int a=72;
-    char star='*';
-    float pi = 3.14;  //theres  no string or boolean data type in c
-    printf("The value of a is %d\n",a);  // \n is used to print in new line
-    printf("The value of star is %c\n",star);
-    printf("The value of pi is %f\n",pi);
-    return 0 ;
-} 
+// int main(){
+//     int a=72;
+//     char star='*';
+//     float pi = 3.14;  //theres  no string or boolean data type in c
+//     printf("The value of a is %d\n",a);  // \n is used to print in new line
+//     printf("The value of star is %c\n",star);
+//     printf("The value of pi is %f\n",pi);
+//     return 0 ;
+// } 
 //%d is used to print integer values, %c is used to print character values, %f is used to print float values-FORMAT SPECIFIERS
+
+//2nd sept
+
+// #include <stdio.h>
+
+// int main(){
+//     int a, b, c;
+//     printf("enter 2 numbers \t");
+//     scanf("%d", &a);
+//     scanf("%d", &b);
+//     printf("sum of a & b is %d" , a+b);
+
+//     return 0;
+
+// }
+
+// #include <stdio.h>
+// int main(){
+//     float r ;
+//     printf("enter radius of circle \n");
+//     scanf("%f", &r);
+//     printf("area of circle is %f", 3.14*r*r);
+//     return 0;
+    
+// }
+// #include <stdio.h>
+// int main(){
+//     int a=1.9999;
+//     printf("the value of a is %d",a);
+//     return 0;  
+// }
+
+#include <stdio.h>
+int main(){
+    int n= 5*2 - 2*3;
+    printf("value of n is %d", n);
+    return 0;
+}
